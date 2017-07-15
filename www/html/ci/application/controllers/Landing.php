066208612f7631a7b8e24c26053b041dbed023c4
Ken Torii <ken.torii7@gmail.com>
2017-07-15 08:42:43 -0400

    clean up

--- www/html/ci/application/controllers/Landing.php
+++ www/html/ci/application/controllers/Landing.php
@@ -20,27 +20,8 @@ class Landing extends CI_Controller {
 	
 	public function index() {
 		
-		
-
-		
-		// get data to send to the view from database
-		$database   = "vizmvp";
-		$query      = $this->db->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE='BASE TABLE'");
-        $tables     = $query->result_array(PDO::FETCH_COLUMN);
-		
-		$info = "";
-		$database_data = array();
-
-        if (empty($tables)) {
-            $info = "<p>There are no tables in database \"{$database}\".</p>";
-        } else {
-            $info = "<p>Database \"{$database}\" has the following tables:</p>";
-            $database_data = $tables;
-        }
-
 		$data = array(
 			'raw_html' => $info,
-			'tables' => $database_data,
 			'base_url' => base_url(),
 		);
 
